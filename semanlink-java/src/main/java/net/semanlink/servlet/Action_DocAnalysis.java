af9e69d743b1411bbb9dbb73229543cf280a57c6
François-Paul Servant <fps@semanlink.net>
2020-03-30 16:12:02 +0200

    post redirect in DocAnalysis

--- semanlink-java/src/main/java/net/semanlink/servlet/Action_DocAnalysis.java
+++ semanlink-java/src/main/java/net/semanlink/servlet/Action_DocAnalysis.java
@@ -2,6 +2,7 @@ package net.semanlink.servlet;
 import javax.servlet.http.*;
 import org.apache.struts.action.*;
 import net.semanlink.semanlink.*;
+import net.semanlink.util.Util;
 import net.semanlink.metadataextraction.*;
 /**
  * Action demandant de (re)faire l'extraction de metadata d'un document
@@ -10,7 +11,6 @@ import net.semanlink.metadataextraction.*;
 public class Action_DocAnalysis extends BaseAction {
 
 public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) {
-	ActionForward x = null;
 	try {
 		String docuri = request.getParameter("uri");
 		if (docuri == null) docuri = request.getParameter("docuri"); // TODO VIRER non ? oh non, c'est l'autre !!
@@ -20,11 +20,29 @@ public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServlet
 		if (extr != null) {
 			extr.doIt(doc, mod);
 		}
-		getJsp_Document(doc, request);
-		x = mapping.findForward("continue");
+		
+		// 2020-03 hum, compare with Action_Download -- different,
+		// with one effect; here, we end up with a url of the form docanalysis.do?docuri=http%3A%2F
+		// but with one less redirect
+		// Doing now as in Action_Download
+		// looks like the "POST RDIRECT" had been neglected here
+		
+//		getJsp_Document(doc, request);
+//		ActionForward x = mapping.findForward("continue");
+		
+		String redirectURL = null;
+		if (docuri.startsWith(SLServlet.getServletUrl())) {
+			redirectURL = docuri; // hum pb de session, non ?
+		} else {
+			// pre uris for bookmarks
+			redirectURL = Util.getContextURL(request) + HTML_Link.docLink(docuri);
+		}
+		
+  	response.sendRedirect(response.encodeRedirectURL(redirectURL));
+  	return null;
+		
 	} catch (Exception e) {
 	    return error(mapping, request, e );
 	}
-	return x;
 } // end execute
 } // end Action
