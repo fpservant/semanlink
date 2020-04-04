358ff9e11900497862ffbd1c8fb83d54c8f56dba
François-Paul Servant <fps@semanlink.net>
2020-04-05 01:14:53 +0200

    AHo-Corasick test: extracting kws from a text

--- semanlink-java/src/test/java/ahocorasick/AhoCorasicTest.java
+++ semanlink-java/src/test/java/ahocorasick/AhoCorasicTest.java
@@ -1,20 +1,24 @@
 /* Created on Apr 4, 2020 */
 package ahocorasick;
 
-import static org.junit.Assert.*;
-
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
 
 import org.ahocorasick.trie.Emit;
 import org.ahocorasick.trie.PayloadEmit;
-import org.ahocorasick.trie.Trie;
 import org.ahocorasick.trie.PayloadTrie;
 import org.ahocorasick.trie.PayloadTrie.PayloadTrieBuilder;
+import org.ahocorasick.trie.handler.EmitHandler;
+import org.ahocorasick.trie.handler.PayloadEmitHandler;
+import org.ahocorasick.trie.handler.StatefulPayloadEmitHandler;
+import org.ahocorasick.trie.Trie;
 import org.apache.jena.rdf.model.Literal;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
@@ -30,13 +34,14 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import net.semanlink.semanlink.SLKeyword;
 import net.semanlink.semanlink.SLSchema;
 import net.semanlink.skos.SKOS;
-import net.semanlink.sljena.JKeyword;
 import net.semanlink.sljena.ModelFileIOManager;
 import net.semanlink.util.index.MultiLabelGetter;
 import net.semanlink.util.text.CharConverter;
+
+// /semanlink/tag/aho_corasick_algorithm.html
+// https://github.com/robert-bor/aho-corasick
 
 public class AhoCorasicTest {
 
@@ -143,7 +148,7 @@ Model getKWsModel() {
 
 
 //PayloadTrie requires aho-corasick more than 0.4.0. ok with 0.6.0
-@Test public final void testWthRealKwsModel() {
+@Test public final void testWithRealKwsModel() {
 	
 	// we do not use ignoreCase, because we also want to handle diacritics.
 	// So we store normalized (lowercase wo diacritics) labels in the Trie,
@@ -180,10 +185,93 @@ Model getKWsModel() {
  // convert the text to the normalized form
 	text = converter.convert(text);
 	Collection<PayloadEmit<Resource>> emits = trie.parseText(text);
+	HashSet<Resource> tags = new HashSet<>();
 	for (PayloadEmit<Resource> emit : emits) {
-		System.out.println(emit.getKeyword() + " : " + emit.getPayload() + " pos: " + emit.getStart() + "/" + emit.getEnd());
+		// System.out.println(emit.getKeyword() + " : " + emit.getPayload() + " pos: " + emit.getStart() + "/" + emit.getEnd());
+		tags.add(emit.getPayload());
 	}
+	ArrayList<Resource> tagsList = new ArrayList<>();
+	tagsList.addAll(tags);
+	for (Resource tag : tagsList) {
+		System.out.println(tag);
+	}
+	
+	
 }
+
+// variation on testWithRealKwsModel
+// in the way we would use it in semanlink: extracting kws
+// from a text
+// Pas réussi à faire marcher un custom emit handler
+// comme je voulais : ignoreOverlaps et onlyWHoleWOrds KO
+
+//// DOES4NT WORK AS EXPECTED: ne prend pas en compte le ignoreOverlaps
+//// et le onlyWholeWords
+//@Test public final void testExtractKWsFromText() {
+//	
+//	// we do not use ignoreCase, because we also want to handle diacritics.
+//	// So we store normalized (lowercase wo diacritics) labels in the Trie,
+//	// and we also normalize the text we want to scan.
+//	
+//	PayloadTrieBuilder<Resource> trieBuilder = PayloadTrie.builder();
+//	trieBuilder.ignoreOverlaps()
+//			// .ignoreCase()
+//			.onlyWholeWords();
+//	
+//	Model kwsModel = getKWsModel();
+//	KwResLabelGetter labelGetter = new KwResLabelGetter();
+//	CharConverter converter = new CharConverter(Locale.FRENCH);
+//	ResIterator kws = kwsModel.listSubjectsWithProperty(RDF.type, kwsModel.getResource(SLSchema.Tag.getURI()));
+//	
+//	int klab = 0, kkw = 0;
+//	for(;kws.hasNext();) {
+//		Resource kw = kws.next();
+//		kkw++;
+//		Iterator<String> labs = labelGetter.getLabels(kw);
+//		for (;labs.hasNext();) {
+//			String lab = labs.next();
+//			// convert the labels to a normalized form
+//			lab = converter.convert(lab);
+//			klab++;
+//			trieBuilder.addKeyword(lab, kw);
+//		}
+//	}
+//	
+//	System.out.println("Nb labels: " + klab + " nb kws: " + kkw);
+//
+//	PayloadTrie<Resource> trie = trieBuilder.build();
+//	
+// // convert the text to the normalized form
+//	text = converter.convert(text);
+//	
+//	HashSet<Resource> tags = new HashSet<>();
+//	
+//	StatefulPayloadEmitHandler<Resource> emitHandler  = new StatefulPayloadEmitHandler<Resource>() {
+//    @Override
+//    public boolean emit(PayloadEmit<Resource> emit) {
+//    	tags.add(emit.getPayload());
+//    	return true;
+//    }
+//
+//		@Override
+//		public List<PayloadEmit<Resource>> getEmits() {
+//			// TODO Auto-generated method stub
+//			return null;
+//		}
+//	};
+//
+//	Collection<PayloadEmit<Resource>> emits = trie.parseText(text, emitHandler);
+//	trie.parseText(text, emitHandler);
+//
+//	for (Resource res : tags) {
+//		System.out.println(res);
+//	}
+//}
+//
+
+//
+//
+//
 
 Model loadKwsModel() throws JenaException, IOException {
 	Model kwsModel = ModelFactory.createDefaultModel();
