7ba54d5b03485df64a6653579480b065d8c44fa4
François-Paul Servant <fps@semanlink.net>
2020-09-30 17:46:56 +0200

    upload: patch empty file type for ".md"

--- semanlink-java/src/main/java/net/semanlink/servlet/UploadServlet.java
+++ semanlink-java/src/main/java/net/semanlink/servlet/UploadServlet.java
@@ -18,64 +18,60 @@ import javax.servlet.http.Part;
 import net.semanlink.util.CopyFiles;
 
 // @WebServlet("sl/upload")
+
+/**
+ * either upload of a file, or creation of a subdir 
+ */
 @MultipartConfig
 public class UploadServlet extends HttpServlet {
 
   @Override
   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
   	try {
-      Part file = request.getPart("file");
-      String filename = getFilename(file);
-      InputStream filecontent = file.getInputStream();
-      // ... Do your file saving job here.
-      
-      // dir to upload to. Either a param of request, or goodDirToSaveAFile
-      File dir = null;
-      String uploadDirUri = request.getParameter("uploadDirUri");
-      if ((uploadDirUri != null) && (!"".equals(uploadDirUri))) {
-      	try {
-					dir = SLServlet.getSLModel().getFile(uploadDirUri);
-					if (!dir.exists()) {
-						throw new RuntimeException("no such dir " + uploadDirUri);
-					}
-				} catch (URISyntaxException e) {
-					throw new RuntimeException(e);
-				}
-      }
-      if (dir == null) {
-      	dir = SLServlet.getSLModel().goodDirToSaveAFile();
-      }
-
-      File f = new File(dir, filename);
-      if (f.exists()) {
-      	throw new RuntimeException("file already exists, no overwrite"); // TODO
-      }
-      
       response.setContentType("text/plain");
       response.setCharacterEncoding("UTF-8");
+  		
+      // dir to upload to. Either a param of request, or goodDirToSaveAFile
+      File dir = toDir(request);
 
-      String boolean_createSubDir = request.getParameter("boolean_createSubDir");
-      if ((boolean_createSubDir != null) && (boolean_createSubDir.equals("true"))) {
-      	// create a subdir
-      	boolean done = f.mkdir();
-      	if (done) {
-          response.getWriter().write("Dir " + filename + " successfully created");
+      // subdir creation: if createSubDir param (supposed to contain name of subdir to be created)
+      String createSubDir = request.getParameter("createSubDir");
+      if (createSubDir != null) {
+      	File f = new File (dir, createSubDir);
+      	if (f.exists()) {
+      		throw new RuntimeException("Already exixts: " + f.getName());
       	} else {
-      		response.setStatus(500);
-      		response.getWriter().write("Dir " + filename + " not created");      		
+        	boolean done = f.mkdir();
+        	if (done) {
+            response.getWriter().write("Dir " + f.getName() + " successfully created");
+        	} else {
+        		response.setStatus(500);
+        		response.getWriter().write("Dir " + f.getName() + " not created");      		
+        	}
       	}
-      	
       } else {
+      	// file upload
+    		Part file = request.getPart("file");
+        if (file == null) {      	
+          	throw new RuntimeException("No 'file' part"); // TODO     
+        }
+        String filename = getFilename(file);
+        InputStream filecontent = file.getInputStream();
+        
+        File f = new File(dir, filename);
+        if (f.exists()) {
+        	throw new RuntimeException("file already exists, no overwrite"); // TODO
+        }
+      	
       	// real upload
         CopyFiles.writeIn2Out(filecontent, new BufferedOutputStream(new FileOutputStream(f)), new byte[5000]);
         
         // System.out.println("filename " + filename + "sub_dir:" + request.getParameter("sub_dir")); // 2020-09 TODO
         response.getWriter().write("File " + filename + " successfully uploaded");
       }
-      
-  	} catch (Exception e) {
+   	} catch (Exception e) {
       // response.sendError(400, "Failed upload: " + e.getMessage());
-      response.setStatus(400);
+      response.setStatus(400); // TODO
       response.setContentType("text/plain");
       response.setCharacterEncoding("UTF-8");
       response.getWriter().write("Failed upload: " + e.getMessage());  		
@@ -90,5 +86,25 @@ public class UploadServlet extends HttpServlet {
           }
       }
       return null;
+  }
+  
+  // dir to upload to. Either a param of request, or goodDirToSaveAFile
+  private File toDir(HttpServletRequest request) throws IOException {
+  	File dir = null;
+    String uploadDirUri = request.getParameter("uploadDirUri");
+    if ((uploadDirUri != null) && (!"".equals(uploadDirUri))) {
+    	try {
+				dir = SLServlet.getSLModel().getFile(uploadDirUri);
+				if (!dir.exists()) {
+					throw new RuntimeException("no such dir " + uploadDirUri);
+				}
+			} catch (URISyntaxException e) {
+				throw new RuntimeException(e);
+			}
+    }
+    if (dir == null) {
+    	dir = SLServlet.getSLModel().goodDirToSaveAFile();
+    }
+    return dir;
   }
 }
